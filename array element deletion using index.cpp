b3d689e1f44ce00adde7c946091f32de2559ea50
Soumyadeep Sinha <110376227+sinhasoumyadeep1234@users.noreply.github.com>
2022-10-17 12:55:15 +0530

    Update array element deletion using index.cpp

--- array element deletion using index.cpp
+++ array element deletion using index.cpp
@@ -20,9 +20,9 @@ void del(int a[],int position){
 	int j,item;
 	item=a[position];		/*taking the element from its index value*/
 	for(j=position;j<n;j++){
-		a[j]=a[j+1];
+		a[j]=a[j+1];		/*swapping the item with other elements*/
 	}
-	n=n-1;
+	n=n-1;				/*reducing the array size*/
 	printf("\nArray after deletion is\n");
 	for(i=0;i<n;i++){
 		printf("\t%d",a[i]);
