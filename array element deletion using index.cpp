368b5e2fb153f4f79db9f1552601511c25e3a590
Soumyadeep Sinha <110376227+sinhasoumyadeep1234@users.noreply.github.com>
2022-10-17 12:54:06 +0530

    Deletion of an array element using its index.

---
+++ array element deletion using index.cpp
@@ -0,0 +1,30 @@
+/*c program to delete an array element using its index value*/
+
+#include<stdio.h>
+void del(int a[],int n);
+int i,n;
+int main(){
+	int position;
+	printf("Enter the array size:");
+	scanf("%d",&n);
+	int a[n];
+	for(i=0;i<n;i++){
+		printf("\nEnter the %d th element of the array:",i);
+		scanf("%d",&a[i]);
+	}
+	printf("\nEnter the element's position that is to be deleted:");
+	scanf("%d",&position);
+	del(a,position);
+}
+void del(int a[],int position){
+	int j,item;
+	item=a[position];		/*taking the element from its index value*/
+	for(j=position;j<n;j++){
+		a[j]=a[j+1];
+	}
+	n=n-1;
+	printf("\nArray after deletion is\n");
+	for(i=0;i<n;i++){
+		printf("\t%d",a[i]);
+	}
+}
